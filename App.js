2c3f00b419c130d946f1c132f5b523a9f9a38020
Ting Zhou <limtingzhou@gmail.com>
2019-07-17 17:19:47 +0800

    Initial commit.

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ReactUsingHook from "./src/ReactUsingHook";
+import ReactUsingClass from "./src/ReactUsingClass";
+import RefreshControlTest from "./src/RefreshControlTest";
+
+const App = () => {
+  return (
+    // <ReactUsingHook />
+    // <ReactUsingClass />
+    <RefreshControlTest />
+  );
+};
+
+export default App;
